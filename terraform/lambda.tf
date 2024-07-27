8c06366c9f09464c1f5452e4eae1d8f21fb9fc77
adrianbp <30375651+adrianbp@users.noreply.github.com>
2024-07-27 14:33:34 +0000

    test

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -28,6 +28,48 @@ resource "aws_lambda_function" "cognito_lambda" {
   handler       = "lambda_function.lambda_handler"
   runtime       = "python3.8"
 
-    environment {
-        variables = {
-        USER_POOL_ID = "${var.u
+  environment {
+    variables = {
+      USER_POOL_ID = "${var.user_pool_id}"
+      CLIENT_ID    = "${var.client_id}"
+    }
+  }
+
+}
+
+resource "aws_api_gateway_rest_api" "api_gateway" {
+  name        = "api_gateway"
+  description = "API Gateway for Lambda function"
+
+  endpoint_configuration {
+    types = ["REGIONAL"]
+  }
+}
+
+resource "aws_api_gateway_resource" "api_gateway_resource" {
+  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
+  parent_id   = aws_api_gateway_rest_api.api_gateway.root_resource_id
+  path_part   = "my-resource"
+}
+
+resource "aws_api_gateway_method" "api_gateway_method" {
+  rest_api_id   = aws_api_gateway_rest_api.api_gateway.id
+  resource_id   = aws_api_gateway_resource.api_gateway_resource.id
+  http_method   = "POST"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "api_gateway_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.api_gateway.id
+  resource_id             = aws_api_gateway_resource.api_gateway_resource.id
+  http_method             = aws_api_gateway_method.api_gateway_method.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.cognito_lambda.invoke_arn
+}
+
+resource "aws_api_gateway_deployment" "api_gateway_deployment" {
+  depends_on = [aws_api_gateway_integration.api_gateway_integration]
+  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
+  stage_name  = "prod"
+}
